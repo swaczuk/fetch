cfcf17a4e9d8b27a180aca5580317bb10b62ce58
Angie Diaz <yelunchi@gmail.com>
2019-08-09 17:47:19 -0300

    agrego maquetado home

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-fetch('https://reqres.in/api/user')
-    .then(response => response.json())
-    .then(res => {
-        // console.log(res.data);
-        const {data} = res
-        data.map(({id, name}) => console.log(`id: ${id}\nname: ${name}`))
-    })
-
- fetch('https://raw.githubusercontent.com/swaczuk/fetch/master/cv.json')
+fetch('https://raw.githubusercontent.com/swaczuk/fetch/master/cv.json')
 .then (response => response.json()
 .then (res => console.log(res)))
